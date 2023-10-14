651ba851fd34dad0113763ba8e205e840de2fe3c
Skyler Adams <skyler14adams@gmail.com>
2023-10-14 15:14:53 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Table_of_Powers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Squares and Cubes table");
+            Console.WriteLine();
+            string choice = "y";
+            while (choice.Equals("y"))
+            {
+                Console.WriteLine("Enter an integer: ");
+                int integer = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                Console.WriteLine("Number\tSquared\tCubed");
+                Console.WriteLine("======\t=======\t=====");
+                for (int i = 1; i <= integer; i++)
+                {
+                    Console.WriteLine(i + "\t" + i * i + "\t" + i * i * i);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Continue? (y/n): ");
+                choice = Console.ReadLine();
+                Console.WriteLine();
+            }
+        }
+    }
+}
